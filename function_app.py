f900d959d2e71fb220ed878c1c793a63d150a315
Mathieu <mathieuducharme86@gmail.com>
2024-01-02 23:04:33 -0500

    change time trigger

--- function_app.py
+++ function_app.py
@@ -4,12 +4,12 @@ import requests
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="* * * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 0 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def playcount_trigger(myTimer: func.TimerRequest) -> None:
     # if myTimer.past_due:
     #     logging.info('The timer is past due!')
 
     response = requests.get('https://spotify-playcount.livelyocean-7de1f403.canadacentral.azurecontainerapps.io/album_queue/populate')
-    
+
     logging.info('The album queue populator endpoint has been called')
