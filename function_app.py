d4418dbf986544c42d56eb1beaca13c68f99b887
Mathieu <mathieuducharme86@gmail.com>
2024-01-21 19:19:11 -0500

    Merge & Pull

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ import requests
 app = func.FunctionApp()
 
 @app.schedule(schedule="0 0 * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
+              use_monitor=False, ) 
 def playcount_trigger(myTimer: func.TimerRequest) -> None:
     # if myTimer.past_due:
     #     logging.info('The timer is past due!')
