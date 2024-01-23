6ce822e6d6307f81583b373fee5b7f78b451c5c4
Mathieu <mathieuducharme86@gmail.com>
2024-01-23 10:17:45 -0500

    add discovery af

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 import azure.functions as func
 import requests
@@ -7,9 +8,17 @@ app = func.FunctionApp()
 @app.schedule(schedule="0 0 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def playcount_trigger(myTimer: func.TimerRequest) -> None:
-    # if myTimer.past_due:
-    #     logging.info('The timer is past due!')
 
     response = requests.get('https://spotify-playcount.livelyocean-7de1f403.canadacentral.azurecontainerapps.io/album_queue/populate')
 
-    logging.info('The album queue populator endpoint has been called')
+    logging.info(f'The album queue populator endpoint has been called at {datetime.now()} (Montreal time)')
+
+@app.schedule(schedule="0 22 * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def discovery_trigger(myTimer: func.TimerRequest) -> None:
+
+    response = requests.get('https://discover-new-releases.livelyocean-7de1f403.canadacentral.azurecontainerapps.io/discover/populate')
+
+    logging.info(f'The artist queue populator endpoint has been called at {datetime.now()} (Montreal time)')
+
+    
